50d9189234e5f09380889dbcafb4f6e6989b179d
GuangyaZhu <976670365@qq.com>
2024-02-13 22:35:18 +1300

    feat(task): added a TaskController and a route to get all tasks

---
+++ ForsythBarr.Server/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using ForsythBarr.Server.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Task = ForsythBarr.Server.Domain.Models.Task;
+
+namespace ForsythBarr.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TaskController(ITaskService taskService) : ControllerBase
+{
+    [HttpGet(Name = "GetAllTasks")]
+    public IEnumerable<Task> Get()
+    {
+        return  taskService.GetAllTasks();
+    }
+}
